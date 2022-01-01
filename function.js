67f4c5ab3d381d635766bb496a466a42042b8e24
Mukul Gupta <mukulgupta257@gmail.com>
2022-01-02 01:23:54 +0530

    added education tab content

---
+++ function.js
@@ -0,0 +1,18 @@
+
+
+
+document.getElementById('about').addEventListener('click',()=>{
+    document.getElementById('education-qual').style.display='none';
+    document.getElementById('leftscreen').style.display='block'
+    document.getElementById('rightscreen').style.display='flex' 
+})
+
+
+
+
+
+document.getElementById('Education').addEventListener('click',()=>{
+document.getElementById('education-qual').style.display='flex';
+document.getElementById('leftscreen').style.display='none'
+document.getElementById('rightscreen').style.display='none'
+})
