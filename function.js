83ed45b6b177631ce293cadd6754971826422c39
Mukul Gupta <mukulgupta257@gmail.com>
2022-01-02 13:23:40 +0530

    mobile version request updated

--- function.js
+++ function.js
@@ -1,18 +1,43 @@
 
 
-
+// about tab 
 document.getElementById('about').addEventListener('click',()=>{
     document.getElementById('education-qual').style.display='none';
-    document.getElementById('leftscreen').style.display='block'
-    document.getElementById('rightscreen').style.display='flex' 
+    document.getElementById('project').style.display='none';
+    document.getElementById('certificate').style.display='none';
+    document.getElementById('leftscreen').style.display='block';
+    document.getElementById('rightscreen').style.display='flex';
 })
 
 
-
-
-
+// education tab
 document.getElementById('Education').addEventListener('click',()=>{
-document.getElementById('education-qual').style.display='flex';
-document.getElementById('leftscreen').style.display='none'
-document.getElementById('rightscreen').style.display='none'
+    document.getElementById('leftscreen').style.display='none';
+    document.getElementById('rightscreen').style.display='none';
+    document.getElementById('project').style.display='none';
+    document.getElementById('certificate').style.display='none';
+    document.getElementById('education-qual').style.display='flex';
 })
+
+
+// projects tab
+document.getElementById('Projects').addEventListener('click',()=>{
+    document.getElementById('education-qual').style.display='none';
+    document.getElementById('leftscreen').style.display='none';
+    document.getElementById('rightscreen').style.display='none';
+    document.getElementById('certificate').style.display='none';
+    document.getElementById('project').style.display='block';
+})
+
+
+// certification tab 
+document.getElementById('certificates').addEventListener('click',()=>{
+    document.getElementById('education-qual').style.display='none';
+    document.getElementById('leftscreen').style.display='none';
+    document.getElementById('rightscreen').style.display='none';
+    document.getElementById('project').style.display='none';
+    document.getElementById('certificate').style.display='flex';
+})
+
+
+
